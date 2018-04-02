e815c9bb1e63e8fffb5f789e2cfbdb9cea677de6
dcrowder2 <dcrowder2@alaska.edu>
2018-04-01 21:53:33 -0800

    Added cosine distance, coherence and updated euclidean using scypi Added/modified comments to include more information regarding parameter shapes

--- k-means.py
+++ k-means.py
@@ -1,10 +1,10 @@
 # Dakota Crowder
 # k-means clustering on Activity sensor data
-# requires preprocess.py
+# requires scipy and numpy
 
 import random as rng
 import numpy as np
-import math
+from scipy.spatial import distance as dis
 
 # used for the k = 3 labels
 sedentary = [1, 2, 3, 4, 7, 8]
@@ -14,27 +14,51 @@ k_3_label = [sedentary, active, exercising]
 
 
 # picks k random points to be the initial centroids
+# array is a 2d array, (instance, feature)
 def random_centroids(array, k):
-    return rng.sample(array, k)
+    return np.asarray(rng.sample(list(array), k))
 
 
 # calculates a new centroid via the mean instance of the cluster points method
+# clusters is a 3d array, (cluster, instance, feature)
+# centroids is a 2d array, (cluster, features)
 def update_centroids(clusters, centroids):
     centroid_mean = []
-    for j in range(len(centroids)):
+    for i in range(len(centroids)):
         feature_mean = []
-        for i in range(len(centroids[0])):
-            feature_mean.append((np.sum(clusters[:, i])) / len(centroids))
+        for j in range(len(centroids[0])):
+            feature_mean.append((np.sum(clusters[i, :, j])) / len(clusters[i]))
         centroid_mean.append(feature_mean)
     return centroid_mean
 
 
 # Calculates the euclidean distance for a single x for each centroid
+# x being a single instance 1-d, (feature)
+# centroids being a 2d array, (centroid, features)
 def calculate_euclidean_distance(x, centroids):
-    distance = []
-    x_minus_c = 0
-    for i in range(len(x)):
-        x_minus_c += x[i] - centroids[i]
-    distance.append(math.sqrt(x_minus_c**2))
-    return np.asarray(distance)
+    return np.asarray([dis.euclidean(x, c) for c in centroids])
+
+
+# Calculates the cosine distance for a single x for each centroid
+# x being a single instance 1-d, (feature)
+# centroids being a 2d array, (centroid, features)
+def calculate_cosine_distance(x, centroids):
+    return np.asarray([(1 - (dis.cosine(x, c))) for c in centroids])
+
+
+# Calculates the centroid coherence, clusters is a 3d array, (cluster, instance, feature)
+# centroids being a 2d array, (centroid, features)
+def centroid_coherence(clusters, centroids):
+    k = len(centroids)
+    coherence = 0
+    for centroid in centroids:
+        cluster_difference = 0
+        for cluster in clusters:
+            cluster_difference += np.sum((cluster - centroid) ** 2)
+        coherence += cluster_difference
+    return (1/k) * coherence
+
+
+# Calculates the Entropy of labels
+def entropy(labels, clusters):
 
